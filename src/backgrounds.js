70f1581752021fb53a216dd6219ea49508ce2039
Joe Kent EW <897368+itsjoekent@users.noreply.github.com>
2020-04-29 19:32:38 -0400

    Signup page, more backgrounds

--- src/backgrounds.js
+++ src/backgrounds.js
@@ -4,4 +4,14 @@ module.exports = {
     source: '/assets/em-header-original.jpg',
     alt: 'Ed at the Podium',
   },
+  'hoops': {
+    type: 'image',
+    source: '/assets/hoops.png',
+    alt: 'Ed shooting hoops',
+  },
+  'air-flight-89': {
+    type: 'image',
+    source: '/assets/air-flight-89.png',
+    alt: 'Ed wearing his pair of Air Flight ‘89s',
+  },
 };
