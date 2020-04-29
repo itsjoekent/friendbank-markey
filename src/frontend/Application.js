ada1ca12a353d3f5fbb8e6d95396bf85e909a10a
Joe Kent EW <897368+itsjoekent@users.noreply.github.com>
2020-04-28 22:24:57 -0400

    styled components ssr, page type routing

--- src/frontend/Application.js
+++ src/frontend/Application.js
@@ -1,9 +1,33 @@
 import React from 'react';
+import { ThemeProvider } from 'styled-components';
+import { ApplicationContext } from './ApplicationContext';
+import ErrorPage from './pages/Error';
+import Homepage from './pages/Homepage';
+import SignupPage from './pages/Signup';
+import theme from './theme';
+
+function reducer(state, action) {
+  return state;
+}
 
 export default function Application(props) {
+  const [state, dispatch] = React.useReducer(reducer, props);
+
+  const contextValue = {
+    ...state,
+    dispatch,
+  };
+
   return (
-    <div>
-      <h1>{props.title}</h1>
-    </div>
-  )
+    <React.Fragment>
+      <ThemeProvider theme={theme}>
+        <ApplicationContext.Provider value={contextValue}>
+          {(!state || !state.pageType || state.pageType === 'error') && <ErrorPage />}
+          {(state.pageType === 'notfound') && <ErrorPage is404 />}
+          {(state.pageType === 'homepage') && <Homepage />}
+          {(state.pageType === 'signup') && <SignupPage />}
+        </ApplicationContext.Provider>
+      </ThemeProvider>
+    </React.Fragment>
+  );
 }
--- src/frontend/ssr.js
+++ src/frontend/ssr.js
@@ -1,7 +1,21 @@
 import React from 'react';
 import ReactDOMServer from 'react-dom/server';
+import { ServerStyleSheet } from 'styled-components';
 import Application from './Application';
 
 export default function ssr(data) {
-  return ReactDOMServer.renderToString(<Application {...data} />);
+  const sheet = new ServerStyleSheet();
+  let styleTags = null;
+  let html = null;
+
+  try {
+    html = ReactDOMServer.renderToString(sheet.collectStyles(<Application {...data} />));
+    styleTags = sheet.getStyleTags();
+  } catch (error) {
+    return error;
+  } finally {
+    sheet.seal();
+  }
+
+  return { html, styleTags };
 }
