e833706756f3a6402d040a5e8f01f6f8ef814359
Joe Kent EW <897368+itsjoekent@users.noreply.github.com>
2020-04-30 18:35:36 -0400

    Adding analytics and bug fixes

--- src/frontend/GlobalStyle.js
+++ src/frontend/GlobalStyle.js
@@ -1,30 +1,6 @@
 import { createGlobalStyle } from 'styled-components';
 
 const GlobalStyle = createGlobalStyle`
-  @font-face {
-    font-family: 'Museo Sans';
-    src: url('/fonts/MuseoSans700.woff2') format('woff2'),
-      url('/fonts/MuseoSans700.woff') format('woff');
-    font-weight: bold;
-    font-style: normal;
-  }
-
-  @font-face {
-    font-family: 'Museo Sans';
-    src: url('/fonts/MuseoSans300.woff2') format('woff2'),
-      url('/fonts/MuseoSans300.woff') format('woff');
-    font-weight: normal;
-    font-style: normal;
-  }
-
-  @font-face {
-    font-family: 'Laca Text';
-    src: url('/fonts/LacaTextBold.woff2') format('woff2'),
-      url('/fonts/LacaTextBold.woff') format('woff');
-    font-weight: bold;
-    font-style: normal;
-  }
-
   body,
   h1,
   h2,
