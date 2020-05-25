86a82bf9e7651a476c5face521cadae48418c115
Joe Kent EW <897368+itsjoekent@users.noreply.github.com>
2020-05-25 15:07:49 -0400

    indexes

--- src/api/db/setup.js
+++ src/api/db/setup.js
@@ -1,8 +1,20 @@
 module.exports = async function setupDb(db) {
   try {
-    const pages = db.collection('pages');
+    const campaigns = db.collection('campaigns');
+    await campaigns.createIndex({ domains: 1 });
 
+    const pages = db.collection('pages');
     await pages.createIndex({ code: 1 });
+    await pages.createIndex({ campaign: 1 });
+    await pages.createIndex({ createdBy: 1 });
+
+    const signups = db.collection('signups');
+    await signups.createIndex({ email: 1 });
+    await signups.createIndex({ recruitedBy: 1 });
+    await signups.createIndex({ campaign: 1 });
+
+    const users = db.collection('users');
+    await users.createIndex({ email: 1 });
 
     return true;
   } catch (error) {
