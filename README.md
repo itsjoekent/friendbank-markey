b61a721798a7ac0cf28825db52de47c78c2992dd
Joe Kent <897368+itsjoekent@users.noreply.github.com>
2020-07-08 17:25:58 -0400

    Adding new fields and consolidating bsd payload logic

--- README.md
+++ README.md
@@ -17,6 +17,30 @@ You can also run `make unit-test-api` to run the API test suite.
 
 - translation for new fields
 
-- Migration script for Markey
- - Attach users to Markey campaign
- - Add Markey copy to Markey campaign
+- Add new question ballot
+  - BSD
+  - Signup variables
+  - Add to manual contact form and signup flow (_conditional in signup form_)
+  - Add question/options to copy configuration
+  - Update seed, tests
+  - env
+- Add actions field
+  - BSD
+  - Signup variables
+  - Add to dashboard
+  - Update tests
+  - env
+- Feature flag in the Admin config for GOTV mode?
+
+
+Address 500's
+<!-- global.location = { pathname: path };
+global.window = {
+  __CAMPAIGN_COPY: JSON.parse(campaign.copy),
+  __CAMPAIGN_CONFIG: JSON.parse(campaign.config),
+};
+
+const ssrResult = await ssr(path, { db, campaign, ObjectId });
+
+global.location = undefined;
+global.window = undefined; -->
---
+++ src/api/utils/constructBsdSignupPayload.js
@@ -0,0 +1,73 @@
+const BSD_VAN_MAP = require('./markeyVanFields');
+
+const {
+  BSD_SIGNUP_SUPPORT_ID,
+  BSD_SIGNUP_VOLUNTEER_ID,
+  BSD_SIGNUP_BALLOT_ID,
+  BSD_SIGNUP_ACTIONS_ID,
+  BSD_SIGNUP_NOTE_ID,
+
+  BSD_CONTACT_SUPPORT_ID,
+  BSD_CONTACT_VOLUNTEER_ID,
+  BSD_CONTACT_BALLOT_ID,
+  BSD_CONTACT_ACTIONS_ID,
+  BSD_CONTACT_NOTE_ID,
+
+  BSD_SIGNUP_FORM_SLUG,
+  BSD_CONTACT_FORM_SLUG,
+} = process.env;
+
+const SUPPORT_ID = 'SUPPORT_ID';
+const VOLUNTEER_ID = 'VOLUNTEER_ID';
+const BALLOT_ID = 'BALLOT_ID';
+const ACTIONS_ID = 'ACTIONS_ID';
+const NOTE_ID = 'NOTE_ID';
+
+const FIELD_MAP = {
+  [BSD_SIGNUP_FORM_SLUG]: {
+    [SUPPORT_ID]: BSD_SIGNUP_SUPPORT_ID,
+    [VOLUNTEER_ID]: BSD_SIGNUP_VOLUNTEER_ID,
+    [BALLOT_ID]: BSD_SIGNUP_BALLOT_ID,
+    [ACTIONS_ID]: BSD_SIGNUP_ACTIONS_ID,
+    [NOTE_ID]: BSD_SIGNUP_NOTE_ID,
+  },
+  [BSD_CONTACT_FORM_SLUG]: {
+    [SUPPORT_ID]: BSD_CONTACT_SUPPORT_ID,
+    [VOLUNTEER_ID]: BSD_CONTACT_VOLUNTEER_ID,
+    [BALLOT_ID]: BSD_CONTACT_BALLOT_ID,
+    [ACTIONS_ID]: BSD_CONTACT_ACTIONS_ID,
+    [NOTE_ID]: BSD_CONTACT_NOTE_ID,
+  },
+};
+
+module.exports = function constructBsdSignupPayload(signup, formSlug) {
+  const bsdPayload = {
+    email: signup.email || '',
+    firstname: signup.firstName,
+    lastname: signup.lastName,
+    phone: signup.phone,
+    zip: signup.zip,
+  };
+
+  if (signup.supportLevel) {
+    bsdPayload[FIELD_MAP[formSlug][SUPPORT_ID]] = BSD_VAN_MAP.support[signup.supportLevel];
+  }
+
+  if (signup.volunteerLevel) {
+    bsdPayload[FIELD_MAP[formSlug][VOLUNTEER_ID]] = BSD_VAN_MAP.volunteer[signup.volunteerLevel];
+  }
+
+  if (signup.ballotStatus) {
+    bsdPayload[FIELD_MAP[formSlug][BALLOT_ID]] = signup.ballotStatus;
+  }
+
+  if (signup.actions) {
+    bsdPayload[FIELD_MAP[formSlug][ACTIONS_ID]] = signup.actions;
+  }
+
+  if (signup.note) {
+    bsdPayload[FIELD_MAP[formSlug][NOTE_ID]] = signup.note;
+  }
+
+  return bsdPayload;
+}
